062c4ee1c8f3614abde6d04894853416a52eb7fc
shariq <shariqshaikh11042004@gmail.com>
2025-06-26 22:38:43 +0530

    tRPC & Inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
-import Image from "next/image";
-import { prisma } from "@/lib/database";
+"use client";
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
 
 export default function Home() {
-  return <div className="text-3xl">Hello</div>;
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+
+  return (
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button onClick={() => invoke.mutate({ text: "John" })}>
+        Invoke Background Job
+      </Button>
+    </div>
+  );
 }
