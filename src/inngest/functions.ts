75f2f832024a3a3acf0fbeffbd29b3d346d7a0a3
shariq <shariqshaikh11042004@gmail.com>
2025-06-27 14:32:15 +0530

    Added OpenAI API

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,23 @@
 import { inngest } from "./client";
+import { Agent, openai, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+    const codeAgent = createAgent({
+      name: "summarizer",
+      system:
+        "You are an expert next.js developer. You write readable, maintainable code. You provide simple Next.js & React snippets that achieve the solution in minimum lines of code.",
+      model: openai({
+        model: "gpt-4o",
+      }),
+    });
+
+    const { output } = await codeAgent.run(
+      `Write the following snippet:  + ${event.data.value}`
+    );
+
+    return { output };
+  }
 );
