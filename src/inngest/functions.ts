0872289efae2ef9f0e559193a4559c5d6ebd0c9a
shariq <shariqshaikh11042004@gmail.com>
2025-06-27 17:22:21 +0530

    E2B Sandbox & Docker

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import { inngest } from "./client";
-import { Agent, openai, createAgent } from "@inngest/agent-kit";
+import { openai, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("luma-nextjs-test-2");
+
+      return sandbox.sandboxId;
+    });
     const codeAgent = createAgent({
       name: "summarizer",
       system:
@@ -18,6 +25,14 @@ export const helloWorld = inngest.createFunction(
       `Write the following snippet:  + ${event.data.value}`
     );
 
-    return { output };
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+
+      const host = sandbox.getHost(3000);
+
+      return `https://${host}`;
+    });
+
+    return { output, sandboxUrl };
   }
 );
