521c6f2c24a960b09f8361dfeb0e90203838dcaf
shariq <shariqshaikh11042004@gmail.com>
2025-06-28 17:38:39 +0530

    Fragment View

---
+++ src/components/Hint.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip";
+
+interface HintProps {
+  children: React.ReactNode;
+  text: string;
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
+}
+
+export const Hint = ({
+  children,
+  text,
+  align = "center",
+  side = "bottom",
+}: HintProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p>{text}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
