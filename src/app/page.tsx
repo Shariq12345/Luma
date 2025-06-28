073d67b407898becdb0c60054551fefbb7bac870
shariq <shariqshaikh11042004@gmail.com>
2025-06-28 12:48:51 +0530

    Message Feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,39 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
+import { toast } from "sonner";
 
 export default function Home() {
   const trpc = useTRPC();
+  const { data: messages } = useQuery(trpc.messages.getMany.queryOptions());
   const [value, setValue] = useState("");
-  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+  const createMessage = useMutation(
+    trpc.messages.create.mutationOptions({
+      onSuccess: () => {
+        toast.success("Message created successfully!");
+      },
+    })
+  );
 
   return (
     <div className="p-4 max-w-7xl mx-auto space-y-3">
       <Input value={value} onChange={(e) => setValue(e.target.value)} />
-      <Button onClick={() => invoke.mutate({ value: value })}>
+      <Button onClick={() => createMessage.mutate({ value: value })}>
         Invoke Background Job
       </Button>
+      {JSON.stringify(messages, null, 2) && (
+        <pre className="bg-gray-100 p-4 rounded">
+          {JSON.stringify(messages, null, 2)
+            .split("\n")
+            .map((line, index) => (
+              <span key={index} className="block">
+                {line}
+              </span>
+            ))}
+        </pre>
+      )}
     </div>
   );
 }
---
+++ src/modules/messages/server/procedures.ts
@@ -0,0 +1,41 @@
+import { inngest } from "@/inngest/client";
+import { prisma } from "@/lib/database";
+import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { z } from "zod";
+
+export const messagesRouter = createTRPCRouter({
+  create: baseProcedure
+    .input(
+      z.object({
+        value: z.string().min(1, "Message is required"),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const createdMessage = await prisma.message.create({
+        data: {
+          content: input.value,
+          role: "USER",
+          type: "RESULT",
+        },
+      });
+
+      await inngest.send({
+        name: "code-agent/run",
+        data: {
+          value: input.value,
+        },
+      });
+
+      return createdMessage;
+    }),
+
+  getMany: baseProcedure.query(async () => {
+    const messages = await prisma.message.findMany({
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+
+    return messages;
+  }),
+});
